3b8efed3c9d67d851ed773c099379a3cf4619a75
BenCodesForSchool <56059350+BenCodesForSchool@users.noreply.github.com>
2019-10-30 01:17:45 -0400

    Add files via upload

---
+++ KryptoNoteActivity.java
@@ -0,0 +1,103 @@
+package com.example.kryptonote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class KryptoNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kryptonote_layout);
+    }
+
+
+    public void onEncrypt(View v) {
+        try {
+            {
+                EditText keyView = (EditText) findViewById(R.id.key);
+                String k = keyView.getText().toString();
+                EditText dataView = (EditText) findViewById(R.id.data);
+                String d = dataView.getText().toString();
+                Cipher myCipher = new Cipher(k);
+                String cipherString = myCipher.encrypt(d);
+                ((EditText) findViewById(R.id.data)).setText(cipherString);
+
+            }
+
+        }
+        catch(Exception e)
+        {
+         Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+
+    }
+
+    public void onDecrypt(View v) {
+        try {
+            {
+                EditText keyView = (EditText) findViewById(R.id.key);
+                String k = keyView.getText().toString();
+                EditText dataView = (EditText) findViewById(R.id.data);
+                String d = dataView.getText().toString();
+                Cipher myCipher = new Cipher(k);
+                String cipherString = myCipher.decrypt(d);
+                ((EditText) findViewById(R.id.data)).setText(cipherString);
+
+            }
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+    }
+
+    public void onSave(View v) {
+        try {
+            String name = ((EditText) findViewById(R.id.file)).getText().toString();
+            File dir = this.getFilesDir();
+            File file = new File(dir, name);
+            FileWriter fw = new FileWriter(file);
+            fw.write(((EditText) findViewById(R.id.data)).getText().toString());
+            fw.close();
+            Toast.makeText(this, "Note Saved.", Toast.LENGTH_LONG);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+    }
+     public void onLoad(View v)
+     {
+         try {
+             String name = ((EditText) findViewById(R.id.file)).getText().toString();
+             File dir = this.getFilesDir();
+             File file = new File(dir, name);
+             FileReader fr = new FileReader(file);
+             String show = "";
+             for (int c = fr.read(); c!=-1; c = fr.read())
+             {
+                 show += (char) c;
+             }
+             fr.close();
+             ((EditText) findViewById(R.id.data)).setText(show);
+             Toast.makeText(this, "Note Saved.", Toast.LENGTH_LONG);
+         }
+         catch (Exception e)
+         {
+             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+         }
+     }
+
+
+
+}
